a2a66d7b97ea67af36d35d4ed56adc794794e934
oliverhu <khu@apache.org>
2024-03-06 12:57:55 -0800

    [refactor] change stream to a simpler version
    Clean up stuff I don't really understand.. might need to add back later
    for robustness.

--- server/src/main.rs
+++ server/src/main.rs
@@ -7,11 +7,7 @@ use engine::{generate_stream, EngineConfig};
 use axum::response::sse::{Event, Sse};
 use axum_extra::{headers, TypedHeader};
 use futures::stream::Stream;
-use std::{collections::HashMap, convert::Infallible, pin::Pin, task::{Context, Poll}};
-
-use tokio::sync::oneshot::error::TryRecvError;
-use tokio::sync::mpsc::Receiver;
-
+use std::{collections::HashMap, convert::Infallible};
 
 #[derive(Parser, Debug)]
 #[command(long_about = None)]
@@ -87,15 +83,19 @@ async fn gen(
 ) -> Sse<impl Stream<Item = Result<Event, Infallible>>> {
     println!("`{}` connected", user_agent.as_str());
     let prompt = params.get("prompt").cloned().unwrap_or("".to_owned());
-    let (sender, receiver) = tokio::sync::mpsc::channel(1000);
+    let (sender, mut receiver) = tokio::sync::mpsc::channel::<Result<Event, Infallible>>(16);
+    let stream = async_stream::stream! {
+        while let Some(event) = receiver.recv().await {
+            yield event
+        }
+    };
 
     tokio::spawn(async move {
         generate_stream(config, prompt, sender).await;
     });
 
-    let mut reusable_receiver = ReusableReceiver::new(receiver);
-    let str = reusable_receiver.stream();
-    Sse::new(str).keep_alive(
+
+    Sse::new(stream).keep_alive(
         axum::response::sse::KeepAlive::new()
             .text("keep-alive-text"),
     )
@@ -103,81 +103,4 @@ async fn gen(
 
 async fn chat(body: String) -> (StatusCode, String) {
     (StatusCode::CREATED, body)
-}
-
-pub struct ReusableReceiver<T> {
-    receiver: Option<Receiver<T>>,
-    receiver_oneshot: Option<tokio::sync::oneshot::Receiver<Receiver<T>>>,
-}
-
-pub struct ReusableReceiverStream<T> {
-    receiver: Option<Receiver<T>>,
-    sender_oneshot: Option<tokio::sync::oneshot::Sender<Receiver<T>>>,
-}
-
-impl<T> ReusableReceiver<T> where T: Sync + Send + 'static {
-    pub fn new(receiver: Receiver<T>) -> Self {
-        Self {
-            receiver: Some(receiver),
-            receiver_oneshot: None,
-        }
-    }
-
-    pub fn stream(&mut self) -> Pin<Box<dyn Stream<Item=T> + Send + Sync + 'static>> {
-        if self.receiver.is_none() {
-            self.recover_receiver();
-        }
-
-        let (sender_oneshot, receiver_oneshot) = tokio::sync::oneshot::channel();
-
-        self.receiver_oneshot = Some(receiver_oneshot);
-
-        Box::pin(ReusableReceiverStream {
-            receiver: std::mem::take(&mut self.receiver),
-            sender_oneshot: Some(sender_oneshot),
-        })
-    }
-
-    fn recover_receiver(&mut self) {
-        let mut receiver_oneshot = std::mem::take(&mut self.receiver_oneshot)
-            .expect("unexpected situation, need to be fixed");
-
-        loop {
-            match receiver_oneshot.try_recv() {
-                Err(TryRecvError::Closed) => {
-                    return;
-                }
-                Err(TryRecvError::Empty) => {}
-                Ok(receiver) => {
-                    self.receiver = Some(receiver);
-                    return;
-                }
-            }
-        }
-    }
-}
-
-impl<T> Drop for ReusableReceiverStream<T> {
-    fn drop(&mut self) {
-        let receiver = std::mem::take(&mut self.receiver)
-            .expect("unexpected situation, need to be fixed");
-        let sender_oneshot = std::mem::take(&mut self.sender_oneshot);
-        let result = sender_oneshot
-            .expect("unexpected situation, need to be fixed")
-            .send(receiver);
-        if let Err(error) = result {
-            println!("{:?}", error);
-        }
-    }
-}
-
-impl<T> Stream for ReusableReceiverStream<T> {
-    type Item = T;
-
-    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
-        self.receiver
-            .as_mut()
-            .expect("unexpected situation, need to be fixed")
-            .poll_recv(cx)
-    }
 }
